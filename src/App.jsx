43219c29c209676e0d358df61fc19fc5056376d2
nithiishkumarb <nithiishkumarb@gmail.com>
2025-09-09 22:56:57 +0530

    DaisyUI installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import './App.css'
-
+import Navbar from "./components/navbar"
 function App() {
 
   return (
-    <>
+    <div className="text-3xl font-bold underline">
+      <Navbar/>
       <h1>Hi</h1>
-    </>
+    </div>
   )
 }
 
